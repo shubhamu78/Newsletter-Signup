7cd04395129545137e6cb0433773873c0136c76d
Shubham Upadhyay <shubham.upadhyay2929@gmail.com>
2020-05-16 20:14:10 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyparser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname +"/signup.html");
+});
+
+
+app.post("/",function(req,res){
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+
+    var data ={
+        members : [
+            {
+                email_address:email,
+                status : "subscribed",
+                merge_fields :{
+                    FNAME:fname,
+                    LNAME:lname
+                }
+            }
+        ]
+    };
+
+    var json = JSON.stringify(data);
+
+    var options = {
+        url:"https://us18.api.mailchimp.com/3.0/lists/d8b980ff4b",
+        method:"Post",
+        headers:{
+            "Authorization":"shubham c15493481d3e0b2fd45d296d55f13f8f-us18"
+        },
+        body : json
+    };
+   
+    request(options,function(error,response,body){
+        if(error){
+            res.send("Oops Error :(");
+        }
+        else{
+            if(response.statusCode == 200){
+            res.sendFile(__dirname + "/success.html");
+            }
+            else{
+                res.sendFile(__dirname + "/failure.html");
+         
+            }
+        }
+
+    });
+    
+});
+
+app.listen(3000,function(){
+    console.log("Success");
+
+});
+
+
+///Api :  c7e2b888724260070d97763fc8135ec0-us19 
+//Unique-id : 5c3bf789f6
